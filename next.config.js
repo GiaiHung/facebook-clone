42e270558d9757963c3e716e1fa66f1a65201a74
GiaiHung <91544801+GiaiHung@users.noreply.github.com>
2022-08-25 15:38:00 +0700

    a

--- next.config.js
+++ next.config.js
@@ -8,6 +8,7 @@ const nextConfig = {
       'platform-lookaside.fbsbx.com',
       'firebasestorage.googleapis.com',
       'scontent.fsgn8-3.fna.fbcdn.net',
+      'https://facebook-clone-giaihung.vercel.app'
     ],
   },
 }
